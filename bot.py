216518d0dcb85f57411c896df0c1f2bc4a3094f6
ynnlz <yannsantini24@gmail.com>
2026-06-19 19:49:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,6 @@ intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # ✅ CONFIGURATION DES ÉMOJIS ET RÔLES
-# Remplace "💳" par ton émoji personnalisé obtenu avec \:paypal:
 PAYPAL_EMOJI = "<:paypal:1517582845315649751>" 
 
 STAFF_ROLE_ID = 1517487833886228550
